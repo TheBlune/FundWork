c1a7b494235c7fe66cb6e995ef3e99a9e108c4c6
Blune <johannes.horeis@gmx.de>
2021-04-03 12:09:49 +0200

    f

--- docs/script.js
+++ docs/script.js
@@ -1,7 +1,7 @@
 $( document ).ready(function() {
   // Set Links to costs.json here
   //fundworks("https://github.com/TheBlune/FundworksCostTemplate/costs.json");
-  //undworks("https://github.com/TheBlune/FundworksCostTemplate/blob/263212505a68bc9d0e66e3fbf2dc9a9a58dbba1a/costs.json");
-  fundworks("https://raw.githubusercontent.com/TheBlune/FundworksCostTemplate/36e57b32b01ffb91fd07f60bc0edc52cfcaaa842/costs.json");
+  fundworks("https://github.com/TheBlune/FundworksCostTemplate/blob/263212505a68bc9d0e66e3fbf2dc9a9a58dbba1a/costs.json");
+  //fundworks("https://raw.githubusercontent.com/TheBlune/FundworksCostTemplate/36e57b32b01ffb91fd07f60bc0edc52cfcaaa842/costs.json");
   //fundworks("https://cdn.jsdelivr.net/gh/TheBlune/FundworksCostTemplate/costs.json");
 });
