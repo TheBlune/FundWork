55296faea6d7858723fc58640b0669bce24c185d
Blune <johannes.horeis@gmx.de>
2021-04-03 11:57:52 +0200

    Online-test

--- fundworks.js
+++ fundworks.js
@@ -1,8 +1,35 @@
 function fundworks(costURL){
   /*
-  1. Method gets financial data from costURL
-  2. then searches for divs with classes "freport","fpatch" or "fbadge"
-  3. inserts its reports there
+  1. Method searches for elements with classes "freport","fpatch" or "fbadge"
+  2. then gets financial data from costURL
+  3. and inserts its reports into these elements
   */
-  console.log("hello world");
+  if($(".freport").length || $(".fpatch").length || $(".fbadge").length) {
+    console.log(costURL);
+    $.getJSON( costURL, function( data ) {
+      console.log(data)
+    })
+      .done(function (data) {
+        console.log(data);
+        $(".freport").append(getReport(data));
+        $(".fpatch").append(getPatch(data));
+        $(".fbadge").append(getBadge(data));
+      })
+      .fail(function(jqxhr, textStatus, error ) {
+        var err = textStatus + ", " + error;
+        console.log( "Request Failed: " + err );
+      })
+  }
+}
+
+function getReport(data) {
+  //Insert Header
+
+
+}
+function getPatch(data) {
+
+}
+function getBadge(data) {
+
 }
